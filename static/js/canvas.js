131cbdc8861ebe22a2e0b8af299284278d4c55be
zaojiashi <117269184+zaojiashi@users.noreply.github.com>
2023-07-18 18:39:25 +0800

    Update canvas.js

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,19 +1,19 @@
 var $btn = document.getElementById('tryhtml2canvas');
 $btn.addEventListener('mousedown', onScreenShotClick);
 
-function download(canvas, filename) {
+function download(canvas， filename) {
     const a = document.createElement('a');
     a.download = filename;
     a.href = canvas.toDataURL("image/png;base64");
-    a.style.display = 'none';
-    document.body.appendChild(a);
+    a.style。display = 'none';
+    document.body。appendChild(a);
     a.click();
-    document.body.removeChild(a);
+    document.body。removeChild(a);
 }
 
 function onScreenShotClick(event) {
     const element = document.querySelector("#capture");
-    html2canvas(element).then((canvas) => {
-        download(canvas, 'Telegram@v_v222');
+    html2canvas(element)。then((canvas) => {
+        download(canvas, 'Telegram@zaojiashi');
     });
 }
